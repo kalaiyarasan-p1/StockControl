005d9bf5cf63404face1443488a171aa04d27283
kalaiyarasan-p1 <kalaiyarasan.p@krossark.com>
2023-06-17 13:12:40 +0530

    Added method  to return fulfilled customer loads sorted by delivery time

--- LoadManagement/LoadManagement/Services/LoadService.cs
+++ LoadManagement/LoadManagement/Services/LoadService.cs
@@ -4,6 +4,12 @@ namespace LoadManagement.Services
 {
     internal class LoadService
     {
+        /// <summary>
+        ///  Method to check load can be fulfilled by warehouse
+        /// </summary>
+        /// <param name="load"></param>
+        /// <param name="warehouse"></param>
+        /// <returns></returns>
         public static bool CanFulfillLoadFromWarehouse(Load load, Warehouse warehouse)
         {
             foreach (LoadItem loadItem in load.LoadItems)
@@ -18,14 +24,44 @@ namespace LoadManagement.Services
             return true;
         }
 
+        public static IEnumerable<Load> GetCustomerLoadsSortedByDeliveryTime(Customer customer)
+        {
+            var loadslist = GetLoads().Where(c => c.Customer.Id == customer.Id);
+            return loadslist.OrderBy(l => l.DeliveryTime);  
+        }
+
+        /// <summary>
+        /// Method to return fulfilled customer loads sorted by delivery time
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+        public static int GetNumberOfLoadsThatCanBeFulfilled(Customer customer)
+        {
+            List<Load> fulfilledLoads = new();
+            var loads = LoadService.GetCustomerLoadsSortedByDeliveryTime(customer);
+            foreach (var load in loads)
+            {
+                var warehouses = LoadService.GetWarehouses();
+                foreach (var warehouse in warehouses)
+                {
+                    bool isLoadFulfilled = LoadService.CanFulfillLoadFromWarehouse(load, warehouse);
+                    if (isLoadFulfilled)
+                    {
+                        fulfilledLoads.Add(load);
+                    }
+                }
+            }
+
+            return fulfilledLoads.Count;
+        }
+
         private static Stock? GetStockByWarehouseAndFreightItem(Warehouse warehouse, FreightItem freightItem)
         {
             return GetStocks().FirstOrDefault(s => s.Warehouse.WarehouseId == warehouse.WarehouseId && s.FreightItem.FreightItemId == freightItem.FreightItemId);
         }
 
-        private static List<Stock> GetStocks()
-        {
-            return new List<Stock>();
-        }
+        private static List<Stock> GetStocks() => new();
+        private static List<Load> GetLoads() => new();
+        public static List<Warehouse> GetWarehouses() => new();
     }
 }
--- LoadManagement/LoadManagement/Startup.cs
+++ LoadManagement/LoadManagement/Startup.cs
@@ -1,7 +1,11 @@
-﻿public class Startup
-{
+﻿using LoadManagement.LoadModels;
+using LoadManagement.Services;
 
+public class Startup
+{
     public static void Main(string[] args)
     {
+        int fulfilledLoad = LoadService.GetNumberOfLoadsThatCanBeFulfilled(new Customer());
+        Console.WriteLine($"Number of fulfilled loads : {fulfilledLoad}");
     }
 }
