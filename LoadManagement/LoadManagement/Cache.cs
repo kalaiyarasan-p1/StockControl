95e83b069fe883ebbf1ba639d31b36e4ee0e7531
kalaiyarasan-p1 <kalaiyarasan.p@krossark.com>
2023-06-17 15:59:22 +0530

    Added cache with least recently used approach

---
+++ LoadManagement/LoadManagement/Cache.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.Caching;
+
+namespace LoadManagement
+{
+    internal class Cache<TKey,TValue>
+    {
+        private readonly MemoryCache cache;
+        private readonly int capacity;
+
+        public Cache(int capacity)
+        { 
+            this.capacity = capacity;
+            this.cache = new MemoryCache("Cache");
+        }
+
+        public TValue Get(TKey key)
+        {
+            TValue value = (TValue)cache.Get(key.ToString());
+            if (value != null)
+            {
+                cache.Remove(key.ToString());
+                cache.Set(key.ToString(), value, Cache<TKey, TValue>.GetCacheItemPolicy());
+            }
+
+            return value;
+        }
+
+        public void Put(TKey key, TValue value)
+        {
+            cache.Set(key.ToString(), value, Cache<TKey, TValue>.GetCacheItemPolicy());
+
+            if (cache.GetCount() > capacity)
+            {
+                var lruKey = cache.Select(kvp => kvp.Key).LastOrDefault();
+                cache.Remove(lruKey);
+            }
+        }
+
+        private static CacheItemPolicy GetCacheItemPolicy()
+        {
+            return new CacheItemPolicy
+            {
+                Priority = CacheItemPriority.Default,
+                SlidingExpiration = TimeSpan.FromMinutes(10) 
+            };
+        }
+    }
+}
