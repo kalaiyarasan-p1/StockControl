e924626eeebb48bae16732aca1c9c38c82390d3d
kalaiyarasan-p1 <kalaiyarasan.p@krossark.com>
2023-06-17 12:22:11 +0530

    Added  a method  to check load can be fufilled from specific warehouse

---
+++ LoadManagement/LoadManagement/Services/LoadService.cs
@@ -0,0 +1,31 @@
+﻿using LoadManagement.LoadModels;
+
+namespace LoadManagement.Services
+{
+    internal class LoadService
+    {
+        public static bool CanFulfillLoadFromWarehouse(Load load, Warehouse warehouse)
+        {
+            foreach (LoadItem loadItem in load.LoadItems)
+            {
+                Stock? stock = GetStockByWarehouseAndFreightItem(warehouse, loadItem.FreightItem);
+                if (stock == null || stock.StockQuantity < loadItem.Quantity)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static Stock? GetStockByWarehouseAndFreightItem(Warehouse warehouse, FreightItem freightItem)
+        {
+            return GetStocks().FirstOrDefault(s => s.Warehouse.WarehouseId == warehouse.WarehouseId && s.FreightItem.FreightItemId == freightItem.FreightItemId);
+        }
+
+        private static List<Stock> GetStocks()
+        {
+            return new List<Stock>();
+        }
+    }
+}
